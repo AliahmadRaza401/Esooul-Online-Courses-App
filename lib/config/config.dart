773963a68ae64edb99b3b59d7080d345a3c3bc4e
Your Name <you@example.com>
2021-08-23 11:48:54 +0500

    raza

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,1 +1,8 @@
+import 'dart:io';
+
 const BaseUrl = "https://www.esooul.com";
+
+  final headers = {
+    HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+    HttpHeaders.acceptHeader: 'application/json',
+  };
